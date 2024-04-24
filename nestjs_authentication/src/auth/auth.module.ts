34a4003af3558cf64156efb33eeef3a0f1c17ff9
trandung2k1 <trandungitksnb2001@gmail.com>
2024-04-24 23:21:20 +0700

    basic nestjs_authentication

---
+++ nestjs_authentication/src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { UsersService } from 'src/users/users.service';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, UsersService],
+  exports: [AuthService],
+})
+export class AuthModule {}
